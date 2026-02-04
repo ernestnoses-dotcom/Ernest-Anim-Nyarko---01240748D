6e74529bf8f4f0ab59f619fedcb2c55a18f7dc98
ERNESTANIMNYARKO-01240748D <ernestnoses@gmail.com>
2026-02-04 18:48:45 +0000

    Add files via upload

---
+++ main (6).cpp
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+ double averageIntegers() {
+     int sum = 0;
+     int i= 0; 
+
+          sum = sum;
+     }
+     return static_cast<double>(sum);
+}
+
+// Example usage 
+int main() {
+    cin << {0, 1, 2, 3, 4};
+    cout << "Average of integers:" << averageIntegers()<<endl;
+    return 0;
+}
