224888251f4ed4c76dc2a9f6890d0376d57e9635
ERNESTANIMNYARKO-01240748D <ernestnoses@gmail.com>
2026-02-04 20:01:21 +0000

    Add files via upload

---
+++ main (8).cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+ int main() {
+     vector<string> list = {"apple", "banana","cherry", "dragonfruit"};
+     cout << "dragonfruit"<<endl;
+ 
+    return 0;
+    
+}
